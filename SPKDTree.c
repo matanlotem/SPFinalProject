2da8a230b357ef4872963e00866bd659e4ccd182
lavidor <itamar.lavidor@gmail.com>
2017-04-23 17:54:49 +0300

    Update SPKDTree.c

--- SPKDTree.c
+++ SPKDTree.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include "SPPoint.h"
 #include "SPKDArray.h"
-//#include "SPKDTree.h"
+#include "SPKDTree.h"
 #include "SPConfig.h"
 
 /**
@@ -13,28 +13,26 @@
  */
 
 /** Type for defining the tree node **/
-typedef struct kd_tree_node_t {
+struct kd_tree_node_t {
 	SPPoint* data;
 	SPKDTreeNode* left;
 	SPKDTreeNode* right;
 	int dim;
 	double val;
-}  SPKDTreeNode;
+};
 
 
-/**
- */
-SPKDTreeNode* spKDTreeInit(SPPoint** arr, int size){
-	if(arr == NULL || size < 1)
+SPKDTreeNode* spKDTreeInit(SPPoint** arr, int n){
+	if(arr == NULL || n < 1)
 		return NULL;
-	SPKDArray* kdA = spKDArrayInit(arr, size);
+	SPKDArray* kdA = spKDArrayInit(arr, n);
 	if(kdA == NULL)
 		return NULL;
     return spKDTreeInitRecursion(kdA, 0);
 }
 
 SPKDTreeNode* spKDTreeInitRecursion(SPKDArray* kdA, int coorSplit){
-	SPKDTreeNode* newNode = (SPKDTreeNode*) malloc(sizeOf(*newNode));
+	SPKDTreeNode* newNode = (SPKDTreeNode*) malloc(sizeof(*newNode));
 	if(newNode == NULL)
 		return NULL;
 	if(spKDArrayGetSize(kdA) == 1){
@@ -45,7 +43,7 @@ SPKDTreeNode* spKDTreeInitRecursion(SPKDArray* kdA, int coorSplit){
 		newNode->data = (spKDArrayGetArray(kdA))[0];
 	}
 	else{
-		int size = spKDArrayGetSize(kdA);
+		int n = spKDArrayGetSize(kdA);
 		int d = spKDArrayGetDimension(kdA);
 		int i = 0;
 		double maxSpread = 0;
@@ -64,20 +62,20 @@ SPKDTreeNode* spKDTreeInitRecursion(SPKDArray* kdA, int coorSplit){
 		{
 			coorSplit = 1;
 			for(i = 0; i<d; i++){
-				currentSpread = (spKDArrayGetIndicesByDim(kdA, i+1))[size-1] - (spKDArrayGetIndicesByDim(kdA, i+1))[0];
+				currentSpread = spPointGetAxisCoor((spKDArrayGetArray(kdA))[(spKDArrayGetIndicesByDim(kdA, i+1))[n-1]],i) - spPointGetAxisCoor((spKDArrayGetArray(kdA))[(spKDArrayGetIndicesByDim(kdA, i+1))[0]],i);
 				if(maxSpread < currentSpread){
 					maxSpread = currentSpread;
 					coorSplit = i+1;
 				}
 			}
 		}
-		SPKDArray** kdASplit = spKDArraySplit(kdArr, coorSplit);
+		SPKDArray** kdASplit = spKDArraySplit(kdA, coorSplit);
 		newNode->dim = coorSplit;
-		i = size;
-		if(size % 2 == 1)
+		i = n;
+		if(n % 2 == 1)
 			i = i-1;
 		i = (int)(i/2);
-		newNode->val = spKDArrayGetIndicesByDim(kdA, coorSplit))[i];
+		newNode->val = spPointGetAxisCoor((spKDArrayGetArray(kdA))[(spKDArrayGetIndicesByDim(kdA, coorSplit))[i]],coorSplit-1);
 		newNode->data = NULL;
 		newNode->left = spKDTreeInitRecursion(kdASplit[0], coorSplit);
 		newNode->right = spKDTreeInitRecursion(kdASplit[1], coorSplit);
@@ -116,13 +114,13 @@ SPKDTreeNode* getRight(SPKDTreeNode* curr){
 
 int getSplitDimension(SPKDTreeNode* curr){
 	if(curr == NULL)
-		return NULL;
+		return -2;
 	return curr->dim;
 }
 
 double getMedianValue(SPKDTreeNode* curr){
 	if(curr == NULL)
-		return NULL;
+		return 0;
 	return curr->val;
 }
 
@@ -137,3 +135,4 @@ void spKDTreeDestroy(SPKDTreeNode* curr){
         free(curr);
     }
 }
+
